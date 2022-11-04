229d68771653b486f0c2bac42c4d9db4ce3fabe7
Ephraim Binyaminov <efraimbinyaminov16@gmail.com>
2022-11-04 16:44:38 +0200

    Add focus on TodoAddInput

--- src/components/TodoAddInput/TodoAddInput.js
+++ src/components/TodoAddInput/TodoAddInput.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState , useRef } from 'react';
 import { nanoid } from 'nanoid';
 
 import './todoAddInput.scss';
@@ -7,18 +7,33 @@ const TodoAddInput = ({addTask}) => {
 
     const [task , setTask] = useState('');
 
+    const TodoAddInputRef = useRef(null);
+
+	const focusOnTodoAddInput = () => {
+		TodoAddInputRef.current.focus();
+	}
+
     return (
         <div className="todoAddInput">
             <input className="todoAddInput__input" 
+                    ref={TodoAddInputRef}
                     type="text" 
                     placeholder="New task"
                     onChange={(e) => setTask(e.target.value)} 
                     value={task}
+                    onKeyPress={(e) => {
+                        if(e.code === "Enter") {
+                            addTask({id: nanoid() , text: task , isChecked: false});
+                            setTask('');
+                            focusOnTodoAddInput();
+                        }
+                    }}
                     />
             <button className="btn todoAddInput__btn"
-                    onClick={() => {
+                    onClick={(e) => {
                         addTask({id: nanoid() , text: task , isChecked: false});
                         setTask('');
+                        focusOnTodoAddInput();
                     }}
                     >Add</button>
         </div>
