71e7662de5d63a23a800f03d5cd6bbb240296092
Ephraim Binyaminov <efraimbinyaminov16@gmail.com>
2022-11-06 17:02:35 +0200

    Minor design fixes

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -51,13 +51,13 @@ const TodoItem = ({id , todoNum , text , isChecked , editTask , deleteTask , tog
     }
 
     const unchecked = (
-        <svg className="todoItem__task_btn-checkbox_icon todoItem__task_btn-checkbox_icon-unchecked" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#000">
+        <svg className="todoItem__task_btn-checkbox_icon todoItem__task_btn-checkbox_icon-unchecked" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="3 3 18 18" fill="#000">
             <path d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"/>
         </svg>
     );
 
     const checked = (
-        <svg className="todoItem__task_btn-checkbox_icon todoItem__task_btn-checkbox_icon-checked" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#000"><path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V5h14v14z"/>
+        <svg className="todoItem__task_btn-checkbox_icon todoItem__task_btn-checkbox_icon-checked" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="3 3 18 18" fill="#000"><path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V5h14v14z"/>
             <path d="M18 9l-1.4-1.4-6.6 6.6-2.6-2.6L6 13l4 4z"/>
         </svg>
     );
